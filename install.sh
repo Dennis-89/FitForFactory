554e45e48735555bcb07ce3eefdb31cdd47644ea
Dennis-89 <straub.dennis1@web.de>
2025-02-22 16:38:30 +0100

    Fix install problems

--- install.sh
+++ install.sh
@@ -8,18 +8,18 @@ fi
 apt update ||
 apt install libglib2.0-dev || dnf install glib2-devel
 
-mkdir -p ~/.local/bin/fitforfactory/.venv
-python -m venv ~/.local/bin/fitforfactory/.venv
+mkdir -p /usr/local/share/applications/fitforfactory/.venv
+python -m venv /usr/local/share/applications/fitforfactory/.venv
 
-git clone https://github.com/Dennis-89/BodyCompositionScale2.git ~/.local/bin/fitforfactory/src/
-~/.local/bin/fitforfactory/.venv/bin/pip install ~/.local/bin/fitforfactory/src
-rm -r ~/.local/bin/fitforfactory/src
-~/.local/bin/fitforfactory/.venv/bin/pip install -m requirements.txt
-cp -r src/* ~/.local/bin/fitforfactory/
-cp src/FitForFactory/Pictures/header.PNG ~/.local/bin/fitforfactory/
+git clone https://github.com/Dennis-89/BodyCompositionScale2.git /usr/local/share/applications/fitforfactory/src/
+/usr/local/share/applications/fitforfactory/.venv/bin/pip install /usr/local/share/applications/fitforfactory/src
+rm -r /usr/local/share/applications/fitforfactory/src
+/usr/local/share/applications/fitforfactory/.venv/bin/pip install -r requirements.txt
+cp -r src/FitForFactory/* /usr/local/share/applications/fitforfactory/
+cp src/FitForFactory/Pictures/header.PNG /usr/local/share/applications/fitforfactory/
+rm -r /usr/local/share/applications/fitforfactory/QtDesigner
 cp FitForFactory.desktop /usr/share/applications/FitForFactory.desktop
 
-chown -c $USER /usr/share/applications/FitForFactory.desktop
-chown -cR $USER ~/.local/bin/fitforfactory/
+chmod +x /usr/share/applications/FitForFactory.desktop
 
 echo -e "${GREEN}Installation erfolgreich! Dieser Ordner kann gelöscht werden.${NC}"
