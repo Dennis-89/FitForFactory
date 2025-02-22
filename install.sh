4578c0ea490a772dd5b0c2a15d58f6ff78becca8
Dennis-89 <straub.dennis1@web.de>
2025-02-22 16:56:47 +0100

    Add Color

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
 if [ "$EUID" -ne 0 ]
   then echo -e "${RED}Datei als root ausführen!${NC}"
   exit
@@ -22,4 +26,4 @@ cp FitForFactory.desktop /usr/share/applications/FitForFactory.desktop
 
 chmod +x /usr/share/applications/FitForFactory.desktop
 
-echo -e "${GREEN}Installation erfolgreich! Dieser Ordner kann gelöscht werden.${NC}"
+echo -e "${GREEN}Installation erfolgreich! Dieser Ordner kann gelöscht werden. Um einfach alle Dateien wieder zu löschen, die <uninstall.sh> nicht löschen.${NC}"
